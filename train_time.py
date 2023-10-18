3bc7679c5c0b9bd1205990dcea3960fff732e0c2
DekuLiuTesla <1369939686@qq.com>
2023-10-18 04:58:49 +0000

    added config file based training

--- train_time.py
+++ train_time.py
@@ -9,6 +9,7 @@
 # For inquiries contact  george.drettakis@inria.fr
 #
 import time
+import yaml
 import os
 import torch
 from random import randint
@@ -21,7 +22,7 @@ import uuid
 from tqdm import tqdm
 from utils.image_utils import psnr
 from argparse import ArgumentParser, Namespace
-from arguments import ModelParams, PipelineParams, OptimizationParams
+from arguments import ModelParams, PipelineParams, OptimizationParams, GroupParams
 try:
     from torch.utils.tensorboard import SummaryWriter
     TENSORBOARD_FOUND = True
@@ -147,11 +148,9 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
 
 def prepare_output_and_logger(args):    
     if not args.model_path:
-        if os.getenv('OAR_JOB_ID'):
-            unique_str=os.getenv('OAR_JOB_ID')
-        else:
-            unique_str = str(uuid.uuid4())
-        args.model_path = os.path.join("./output/", unique_str[0:10])
+        config_name = os.path.splitext(os.path.basename(args.config_path))[0]
+        time_stamp = time.strftime("%Y%m%d%H%M%S", time.localtime(time.time()))
+        args.model_path = os.path.join("./output/", config_name, time_stamp)
         
     # Set up output folder
     print("Output folder: {}".format(args.model_path))
@@ -208,12 +207,26 @@ def training_report(tb_writer, iteration, Ll1, loss, l1_loss, t_render, t_loss, 
             tb_writer.add_scalar('total_points', scene.gaussians.get_xyz.shape[0], iteration)
         torch.cuda.empty_cache()
 
+def parse_cfg(cfg):
+    lp = GroupParams()
+    op = GroupParams()
+    pp = GroupParams()
+
+    for arg in cfg['model_params'].items():
+        setattr(lp, arg[0], arg[1])
+    
+    for arg in cfg['optim_params'].items():
+        setattr(op, arg[0], arg[1]) 
+
+    for arg in cfg['pipeline_params'].items():
+        setattr(pp, arg[0], arg[1])
+    
+    return lp, op, pp
+
 if __name__ == "__main__":
     # Set up command line argument parser
     parser = ArgumentParser(description="Training script parameters")
-    lp = ModelParams(parser)
-    op = OptimizationParams(parser)
-    pp = PipelineParams(parser)
+    parser.add_argument('config', help='train config file path')
     parser.add_argument('--ip', type=str, default="127.0.0.1")
     parser.add_argument('--port', type=int, default=6009)
     parser.add_argument('--debug_from', type=int, default=-1)
@@ -224,9 +237,13 @@ if __name__ == "__main__":
     parser.add_argument("--checkpoint_iterations", nargs="+", type=int, default=[])
     parser.add_argument("--start_checkpoint", type=str, default = None)
     args = parser.parse_args(sys.argv[1:])
-    args.save_iterations.append(args.iterations)
+    with open(args.config) as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+        lp, op, pp = parse_cfg(cfg)
+        setattr(lp, 'config_path', args.config)
+        args.save_iterations.append(op.iterations)
     
-    print("Optimizing " + args.model_path)
+    print("Optimizing " + lp.model_path)
 
     # Initialize system state (RNG)
     safe_state(args.quiet)
@@ -234,7 +251,7 @@ if __name__ == "__main__":
     # Start GUI server, configure and run training
     network_gui.init(args.ip, args.port)
     torch.autograd.set_detect_anomaly(args.detect_anomaly)
-    training(lp.extract(args), op.extract(args), pp.extract(args), args.test_iterations, args.save_iterations, args.checkpoint_iterations, args.start_checkpoint, args.debug_from)
+    training(lp, op, pp, args.test_iterations, args.save_iterations, args.checkpoint_iterations, args.start_checkpoint, args.debug_from)
 
     # All done
     print("\nTraining complete.")
