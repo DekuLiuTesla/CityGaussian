402bffe3fe29948f5d256c2c63c257d2716d19b0
DekuLiuTesla <1369939686@qq.com>
2023-11-27 07:20:55 +0000

    added upper bound test of fusion

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-CONFIG="org_m360_bicycle_grad"
+CONFIG="org_ue_aerial_20_blockFcam"
+
+# python convert.py -s data/UE-collected/aerial/block1_6
+python convert_cam.py -s data/UE-collected/aerial/blockF_20_cam
 
 python train_time.py --config config/$CONFIG.yaml
 python render.py -m output/$CONFIG
