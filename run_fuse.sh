789db77010286ec24b7d75e13b7db8b7b4eb307e
DekuLiuTesla <1369939686@qq.com>
2023-12-03 02:44:20 +0000

    updated run_fuse.sh

--- run_fuse.sh
+++ run_fuse.sh
@@ -1,8 +1,12 @@
-CONFIG="fuse_ue_aerial_20_blockFcam_7k_1lr"
+CONFIG="fuse_ue_aerial_20_blockFcam_7k_reset2k"
 
 # python convert.py -s data/UE-collected/aerial/block1_6
-# python convert_cam.py -s data/UE-collected/aerial/blockF_20_cam
+# python convert_cam.py -s data/UE-collected/aerial/blockF_test20_1f2
 
-python fuse.py --config config/$CONFIG.yaml
-python render_fuse.py --config config/$CONFIG.yaml
-python metrics.py -m output/$CONFIG
+# python fuse.py --config config/$CONFIG.yaml
+python render_fuse.py --config config/$CONFIG.yaml --source_test
+python metrics_custom.py -m output/$CONFIG -t test_src
+
+# python fuse.py --config config/$CONFIG.yaml
+# python render_fuse.py --config config/$CONFIG.yaml
+# python metrics_custom.py -m output/$CONFIG
