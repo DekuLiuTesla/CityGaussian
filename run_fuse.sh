7cff3a87496c690fe21dc516495a691cee70156d
DekuLiuTesla <1369939686@qq.com>
2023-12-08 01:40:12 +0000

    update run_fuse.sh

--- run_fuse.sh
+++ run_fuse.sh
@@ -1,9 +1,10 @@
-CONFIG="fuse_ue_aerial_20_blockFcam_7k"
-
+CONFIG="fuse_ue_aerial_20_block123Fcam_7k"
 python fuse.py --config config/$CONFIG.yaml
-python render_fuse.py --config config/$CONFIG.yaml --custom_test --skip_train --skip_test
-python metrics_custom.py -m output/$CONFIG -t blockF_test20_1f2
 
-# python fuse.py --config config/$CONFIG.yaml
-# python render_fuse.py --config config/$CONFIG.yaml
-# python metrics_custom.py -m output/$CONFIG
+# TEST_PATH="data/UE-collected/street/block2_5_cam"
+python render_fuse.py --config config/$CONFIG.yaml
+python metrics_custom.py -m output/$CONFIG
+
+# TEST_PATH="data/UE-collected/aerial/blockF_test20_1f2"
+# python render_fuse.py --config config/$CONFIG.yaml --custom_test $TEST_PATH  --skip_train --skip_test
+# python metrics_custom.py -m output/$CONFIG -t blockF_test20_1f2
