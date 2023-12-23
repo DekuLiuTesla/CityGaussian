ed6bd660d81531eb4efcc6f1649f83cf7f63fe0c
DekuLiuTesla <1369939686@qq.com>
2023-12-23 12:20:11 +0000

    added train/test for block 9

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
-CONFIG="org_ue_aerial_20_blockFcam"
+CONFIG="org_mc_aerial_block9"
 
-python train_time.py --config config/$CONFIG.yaml
-python render.py -m output/$CONFIG
-python metrics.py -m output/$CONFIG
+python train_large.py --config config/$CONFIG.yaml
+
+TEST_PATH="data/matrix_city/aerial/test/block_9_test"
+python render_fuse.py --config config/$CONFIG.yaml --custom_test $TEST_PATH  --skip_train --skip_test
+python metrics_custom.py -m output/$CONFIG -t block_9_test
+# python render_large.py -m output/$CONFIG
+# python metrics.py -m output/$CONFIG
