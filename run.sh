e9dc64013a2d1d9b0fcec6c8612390f4a43ab3f9
DekuLiuTesla <1369939686@qq.com>
2023-12-29 08:47:43 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 CONFIG="block_mc_aerial_block9"
 
-for num in {0..15}  
-do  
-python train_large.py --config config/$CONFIG.yaml --block_id $num
-done 
+# train coarse global GS
+CUDA_VISIBLE_DEVICES=1 python train_large.py --config config/$CONFIG.yaml
+
+# train GS cell
+# for num in {0..15}  
+# do  
+# python train_large.py --config config/$CONFIG.yaml --block_id $num
+# done 
 
 TEST_PATH="data/matrix_city/aerial/test/block_9_test"
 python render_fuse.py --config config/$CONFIG.yaml --custom_test $TEST_PATH  --skip_train --skip_test
 python metrics_custom.py -m output/$CONFIG -t block_9_test
-
-# python render_large.py -m output/$CONFIG
-# python metrics.py -m output/$CONFIG
