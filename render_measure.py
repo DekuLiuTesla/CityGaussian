9b8cd9096f0560bce7c1b9a637d837e8b2a75fe1
DekuLiuTesla <1369939686@qq.com>
2024-03-03 21:45:09 +0800

    updates code for LoD measure

--- render_measure.py
+++ render_measure.py
@@ -38,8 +38,8 @@ def render_set(model_path, name, iteration, views, gaussians, pipeline, backgrou
     avg_memory = 0
     max_memory = 0
 
-    render_path = os.path.join(model_path, name, "ours_{}".format(iteration), "renders")
-    gts_path = os.path.join(model_path, name, "ours_{}".format(iteration), "gt")
+    render_path = os.path.join(model_path, name, "ours_{}".format(height), "renders")
+    gts_path = os.path.join(model_path, name, "ours_{}".format(height), "gt")
 
     makedirs(render_path, exist_ok=True)
     makedirs(gts_path, exist_ok=True)
--- render_measure_lod.py
+++ render_measure_lod.py
@@ -120,8 +120,8 @@ def render_set(model_path, name, iteration, views, gaussians, pipeline, backgrou
     avg_memory = 0
     max_memory = 0
 
-    render_path = os.path.join(model_path, name, "ours_lod_{}".format(iteration), "renders")
-    gts_path = os.path.join(model_path, name, "ours_lod_{}".format(iteration), "gt")
+    render_path = os.path.join(model_path, name, "ours_lod_{}".format(height), "renders")
+    gts_path = os.path.join(model_path, name, "ours_lod_{}".format(height), "gt")
 
     makedirs(render_path, exist_ok=True)
     makedirs(gts_path, exist_ok=True)
