9b500bc708de6986a3022aec2ad195606b804111
DekuLiuTesla <1369939686@qq.com>
2024-04-06 09:38:19 +0800

    fixed bugs in data processing

--- sh_data_proc.sh
+++ sh_data_proc.sh
@@ -1,8 +1,8 @@
-python tools/transform_json2txt_mc.py --source_path data/matrix_city/aerial_street_fusion/aerial
-python tools/convert_cam.py -s data/matrix_city/aerial_street_fusion/aerial
+# python tools/transform_json2txt_mc.py --source_path data/matrix_city/aerial_street_fusion/aerial
+# python tools/convert_cam.py -s data/matrix_city/aerial_street_fusion/aerial
 
-python tools/transform_json2txt_mc.py --source_path data/matrix_city/aerial_street_fusion/street/train
+python tools/transform_json2txt_mc.py --source_path data/matrix_city/aerial_street_fusion/street/train --intrinsic_path data/matrix_city/street/pose/block_small/transforms_train.json
 python tools/convert_cam.py -s data/matrix_city/aerial_street_fusion/street/train
 
-python tools/transform_json2txt_mc.py --source_path data/matrix_city/aerial_street_fusion/street/test
+python tools/transform_json2txt_mc.py --source_path data/matrix_city/aerial_street_fusion/street/test --intrinsic_path data/matrix_city/street/pose/block_small/transforms_train.json
 python tools/convert_cam.py -s data/matrix_city/aerial_street_fusion/street/test
