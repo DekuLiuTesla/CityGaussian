17d9dc41b6379639c78a6ec5c8a6aaaa4fc2e733
DekuLiuTesla <1369939686@qq.com>
2024-04-09 10:12:30 +0800

    updates merging to Large Scale training

---
+++ merge.py
@@ -0,0 +1,122 @@
+#
+# Copyright (C) 2023, Inria
+# GRAPHDECO research group, https://team.inria.fr/graphdeco
+# All rights reserved.
+#
+# This software is free for non-commercial, research and evaluation use 
+# under the terms of the LICENSE.md file.
+#
+# For inquiries contact  george.drettakis@inria.fr
+#
+
+import torch
+import yaml
+import os
+import sys
+
+import torchvision
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision.transforms.functional as tf
+from PIL import Image
+from tqdm import tqdm
+from os import makedirs
+from scene import Scene
+from gaussian_renderer import render
+from utils.general_utils import safe_state
+from argparse import ArgumentParser
+from arguments import ModelParams, PipelineParams
+from gaussian_renderer import GaussianModel
+from arguments import ModelParams, PipelineParams, OptimizationParams, GroupParams
+from utils.loss_utils import l1_loss, ssim
+
+def parse_cfg(cfg):
+    lp = GroupParams()
+    op = GroupParams()
+    pp = GroupParams()
+
+    for arg in cfg['model_params'].items():
+        setattr(lp, arg[0], arg[1])
+    
+    for arg in cfg['optim_params'].items():
+        setattr(op, arg[0], arg[1]) 
+
+    for arg in cfg['pipeline_params'].items():
+        setattr(pp, arg[0], arg[1])
+    
+    return lp, op, pp
+
+def readImages(renders_dir, gt_dir):
+    renders = []
+    gts = []
+    image_names = []
+    for fname in os.listdir(renders_dir):
+        render = Image.open(renders_dir / fname)
+        gt = Image.open(gt_dir / fname)
+        renders.append(tf.to_tensor(render).unsqueeze(0)[:, :3, :, :].cuda())
+        gts.append(tf.to_tensor(gt).unsqueeze(0)[:, :3, :, :].cuda())
+        image_names.append(fname)
+    return renders, gts, image_names
+
+def blockMerge(lp, iteration):
+    out_dir = lp.model_path
+    merged_gaussians = GaussianModel(lp.sh_degree)
+    num_blocks = lp.block_dim[0] * lp.block_dim[1] * lp.block_dim[2]
+    with torch.no_grad():
+        for idx in range(num_blocks):
+            gaussians = GaussianModel(lp.sh_degree)
+            try:
+                gaussians.load_ply(os.path.join(out_dir, f"cells/cell{idx}", "point_cloud_blocks", "scale_1.0",
+                                                "iteration_" + str(iteration),
+                                                "point_cloud.ply"))
+            except:
+                gaussians.load_ply(os.path.join(out_dir, f"cells/cell{idx}", "point_cloud_blocks", "scale_1.0",
+                                                "iteration_" + str(1),
+                                                "point_cloud.ply"))
+            
+            if len(merged_gaussians._xyz) == 0:
+                merged_gaussians._xyz = gaussians.get_xyz
+                merged_gaussians._features_dc = gaussians._features_dc
+                merged_gaussians._features_rest = gaussians._features_rest
+                merged_gaussians._scaling = gaussians._scaling
+                merged_gaussians._rotation = gaussians._rotation
+                merged_gaussians._opacity = gaussians._opacity
+                merged_gaussians.max_radii2D = gaussians.max_radii2D
+            else:
+                merged_gaussians._xyz = torch.cat([merged_gaussians._xyz, gaussians.get_xyz], dim=0)
+                merged_gaussians._features_dc = torch.cat([merged_gaussians._features_dc, gaussians._features_dc], dim=0)
+                merged_gaussians._features_rest = torch.cat([merged_gaussians._features_rest, gaussians._features_rest], dim=0)
+                merged_gaussians._scaling = torch.cat([merged_gaussians._scaling, gaussians._scaling], dim=0)
+                merged_gaussians._rotation = torch.cat([merged_gaussians._rotation, gaussians._rotation], dim=0)
+                merged_gaussians._opacity = torch.cat([merged_gaussians._opacity, gaussians._opacity], dim=0)
+                merged_gaussians.max_radii2D = torch.cat([merged_gaussians.max_radii2D, gaussians.max_radii2D], dim=0)
+            
+            print(f"Merged {len(gaussians.get_xyz)} points from block {idx}")
+    
+    save_path = os.path.join(out_dir, "point_cloud", "iteration_" + str(iteration), "point_cloud.ply")
+    print(f"Saving merged {len(merged_gaussians.get_xyz)} point cloud to {save_path}")
+    merged_gaussians.save_ply(save_path)
+    print('Done')
+
+
+if __name__ == "__main__":
+    device = torch.device("cuda:0")
+    torch.cuda.set_device(device)
+
+    # Set up command line argument parser
+    parser = ArgumentParser(description="Training script parameters")
+    parser.add_argument('--config', type=str, help='train config file path')
+    parser.add_argument('--model_path', type=str, help='model path of fused model')
+    parser.add_argument("--iteration", default=30_000, type=int)
+    args = parser.parse_args(sys.argv[1:])
+    if args.model_path is None:
+        args.model_path = os.path.join('output', os.path.basename(args.config).split('.')[0])
+
+    with open(args.config) as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+        lp, op, pp = parse_cfg(cfg)
+        setattr(lp, 'config_path', args.config)
+        if lp.model_path == '':
+            lp.model_path = args.model_path
+
+    blockMerge(lp, args.iteration)
--- run_large.sh
+++ run_large.sh
@@ -1,28 +1,32 @@
-CONFIG="org_mc_aerial_block_all_lr2"
-CUDA_VISIBLE_DEVICES=4 python train_large.py --config config/$CONFIG.yaml
+# CONFIG="org_mc_aerial_block3_distorted"
+# CUDA_VISIBLE_DEVICES=4 python train_large.py --config config/$CONFIG.yaml
 
 # train GS cell
-# CONFIG="block_mc_aerial_block_all_lr_c36_loss_5"
-# for num in {0..4}  
-# do
-# let _num=num*8+0
-# CUDA_VISIBLE_DEVICES=0 python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1024 &
-# let _num=num*8+1
-# CUDA_VISIBLE_DEVICES=1 python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1025 &
-# let _num=num*8+2
-# CUDA_VISIBLE_DEVICES=2 python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1026 &
-# let _num=num*8+3
-# CUDA_VISIBLE_DEVICES=3 python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1027 &
-# let _num=num*8+4
-# CUDA_VISIBLE_DEVICES=4 python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1028 &
-# let _num=num*8+5
-# CUDA_VISIBLE_DEVICES=5 python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1029 &
-# let _num=num*8+6
-# CUDA_VISIBLE_DEVICES=6 python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1030 &
-# let _num=num*8+7
-# CUDA_VISIBLE_DEVICES=7 python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1031 &
-# wait
-# done 
+CONFIG="block_mc_aerial_block_all_lr_c36_loss_8_num_10w_lr2"
+CUDA_VISIBLE_DEVICES=0 python data_partition.py --config config/$CONFIG.yaml
+# select num range according to block number
+for num in {0..3}  
+do
+let _num=num*8+0
+CUDA_VISIBLE_DEVICES=0 WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1024 &
+let _num=num*8+1
+CUDA_VISIBLE_DEVICES=1 WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1025 &
+let _num=num*8+2
+CUDA_VISIBLE_DEVICES=2 WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1026 &
+let _num=num*8+3
+CUDA_VISIBLE_DEVICES=3 WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1027 &
+let _num=num*8+4
+CUDA_VISIBLE_DEVICES=4 WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1028 &
+let _num=num*8+5
+CUDA_VISIBLE_DEVICES=5 WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1029 &
+let _num=num*8+6
+CUDA_VISIBLE_DEVICES=6 WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1030 &
+let _num=num*8+7
+CUDA_VISIBLE_DEVICES=7 WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1031 &
+wait
+done
+
+python merge.py --config config/$CONFIG.yaml
 
 TEST_PATH="data/matrix_city/aerial/test/block_all_test"
 CUDA_VISIBLE_DEVICES=3 python render_large.py --config config/$CONFIG.yaml --custom_test $TEST_PATH
