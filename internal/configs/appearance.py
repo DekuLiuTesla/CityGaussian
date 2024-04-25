25fa64e2a6e24b323ad746a951814e936cd380e0
DekuLiuTesla <1369939686@qq.com>
2024-04-25 14:49:40 +0800

    adds swag renderer

--- internal/configs/appearance.py
+++ internal/configs/appearance.py
@@ -20,3 +20,14 @@ class AppearanceModelParams:
     n_frequencies: int = 4
     grayscale_factors_activation: str = "Sigmoid"
     gamma_activation: str = "Softplus"
+
+@dataclass
+class SwagAppearanceModelParams:
+    optimization: AppearanceModelOptimizationParams
+
+    n_appearance_count: int=6000
+    n_appearance_dims: int = 24
+    n_input_dims: int = 30
+    n_neurons: int = 64
+    n_hidden_layers: int = 3
+    color_activation: str = "Sigmoid"
--- internal/renderers/__init__.py
+++ internal/renderers/__init__.py
@@ -1,4 +1,5 @@
 from .renderer import Renderer
 from .vanilla_renderer import VanillaRenderer
 from .appearance_mlp_renderer import AppearanceMLPRenderer
+from .appearance_swag_renderer import AppearanceSWAGRenderer
 from .rgb_mlp_renderer import RGBMLPRenderer
