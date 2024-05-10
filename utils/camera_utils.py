fbfd48cc92b9276edeb185bf1e04a1e738d54065
DekuLiuTesla <1369939686@qq.com>
2024-05-10 18:58:23 +0800

    adds speed measure for trajectory 2 and 3

--- utils/camera_utils.py
+++ utils/camera_utils.py
@@ -216,6 +216,62 @@ def loadCamTA1(args, id, cam_info, resolution_scale, pitch=None, height=None):
                   image=gt_image, gt_alpha_mask=loaded_mask,
                   image_name=cam_info.image_name, uid=id, data_device=args.data_device)
 
+def loadCamTA3(args, id, cam_info, resolution_scale, xyz=None, pitch=None, yaw=None):
+    # use appointed pitch and height
+    orig_w, orig_h = cam_info.image.size
+
+    if args.resolution in [1, 2, 4, 8]:
+        resolution = round(orig_w/(resolution_scale * args.resolution)), round(orig_h/(resolution_scale * args.resolution))
+    else:  # should be a type that converts to float
+        if args.resolution == -1:
+            if orig_w > 1600:
+                global WARNED
+                if not WARNED:
+                    print("[ INFO ] Encountered quite large input images (>1.6K pixels width), rescaling to 1.6K.\n "
+                        "If this is not desired, please explicitly specify '--resolution/-r' as 1")
+                    WARNED = True
+                global_down = orig_w / 1600
+            else:
+                global_down = 1
+        else:
+            global_down = orig_w / args.resolution
+
+        scale = float(global_down) * float(resolution_scale)
+        resolution = (int(orig_w / scale), int(orig_h / scale))
+
+    resized_image_rgb = PILtoTorch(cam_info.image, resolution)
+
+    gt_image = resized_image_rgb[:3, ...]
+    loaded_mask = None
+
+    if resized_image_rgb.shape[1] == 4:
+        loaded_mask = resized_image_rgb[3:4, ...]
+
+    if yaw is not None and pitch is not None and xyz is not None:
+        Rt = np.zeros((4, 4))
+        Rt[:3, :3] = cam_info.R.transpose()
+        Rt[:3, 3] = cam_info.T
+        Rt[3, 3] = 1.0
+
+        C2W = np.linalg.inv(Rt)
+        euler = np.array(mat2euler(C2W[:3, :3]))
+        euler[0] = pitch
+        euler[2] = yaw
+        C2W[:3, :3] = euler2mat(*euler)
+        C2W[:3, 3] = xyz
+        Rt = np.linalg.inv(C2W)
+
+        R = Rt[:3, :3].transpose()
+        T = Rt[:3, 3]
+    else:
+        R = cam_info.R
+        T = cam_info.T
+
+    return Camera(colmap_id=cam_info.uid, R=R, T=T, 
+                  FoVx=cam_info.FovX, FoVy=cam_info.FovY, 
+                  image=gt_image, gt_alpha_mask=loaded_mask,
+                  image_name=cam_info.image_name, uid=id, data_device=args.data_device)
+
 
 def cameraList_from_camInfos(cam_infos, resolution_scale, args):
     camera_list = []
