85e3d79e466c489bde30acd5e3b993d07bd07c10
DekuLiuTesla <1369939686@qq.com>
2024-05-30 10:02:39 +0800

    updates full pipeline before LoD

--- run_large.sh
+++ run_large.sh
@@ -1,33 +1,44 @@
-# CONFIG="org_mc_aerial_block3_distorted"
-# CUDA_VISIBLE_DEVICES=4 python train_large.py --config config/$CONFIG.yaml
+get_available_gpu() {
+  local mem_threshold=5000
+  nvidia-smi --query-gpu=index,memory.used --format=csv,noheader,nounits | awk -v threshold="$mem_threshold" -F', ' '
+  $2 < threshold { print $1; exit }
+  '
+}
 
-# train GS cell
-CONFIG="block_mc_aerial_block_all_lr_c36_loss_8_num_10w_lr2"
-CUDA_VISIBLE_DEVICES=0 python data_partition.py --config config/$CONFIG.yaml
-# select num range according to block number
-for num in {0..3}  
-do
-let _num=num*8+0
-CUDA_VISIBLE_DEVICES=0 WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1024 &
-let _num=num*8+1
-CUDA_VISIBLE_DEVICES=1 WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1025 &
-let _num=num*8+2
-CUDA_VISIBLE_DEVICES=2 WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1026 &
-let _num=num*8+3
-CUDA_VISIBLE_DEVICES=3 WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1027 &
-let _num=num*8+4
-CUDA_VISIBLE_DEVICES=4 WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1028 &
-let _num=num*8+5
-CUDA_VISIBLE_DEVICES=5 WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1029 &
-let _num=num*8+6
-CUDA_VISIBLE_DEVICES=6 WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1030 &
-let _num=num*8+7
-CUDA_VISIBLE_DEVICES=7 WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $_num --port 1031 &
-wait
+# train coarse global gaussian model
+CONFIG="org_mc_aerial_block_all_lr2"
+CUDA_VISIBLE_DEVICES=$(get_available_gpu) python train_large.py --config config/$CONFIG.yaml
+
+# train CityGaussian
+CONFIG="block_mc_aerial_block_all_lr_c36_loss_8"
+# obtain data partitioning
+CUDA_VISIBLE_DEVICES=$(get_available_gpu) python data_partition.py --config config/$CONFIG.yaml
+
+# optimize each block
+port=4041
+for num in {0..8}; do
+    while true; do
+        gpu_id=$(get_available_gpu)
+        if [[ -n $gpu_id ]]; then
+            echo "GPU $gpu_id is available. Starting training block '$num'"
+            CUDA_VISIBLE_DEVICES=$gpu_id WANDB_MODE=offline python train_large.py --config config/$CONFIG.yaml --block_id $num --port $port &
+            # Increment the port number for the next run
+            ((port++))
+            # Allow some time for the process to initialize and potentially use GPU memory
+            sleep 180
+            break
+        else
+            echo "No GPU available at the moment. Retrying in 3 minute."
+            sleep 180
+        fi
+    done
 done
+wait
 
-python merge.py --config config/$CONFIG.yaml
+# merge the blocks
+CUDA_VISIBLE_DEVICES=$(get_available_gpu) python merge.py --config config/$CONFIG.yaml
 
-TEST_PATH="data/matrix_city/aerial/test/block_all_test"
-CUDA_VISIBLE_DEVICES=3 python render_large.py --config config/$CONFIG.yaml --custom_test $TEST_PATH
-CUDA_VISIBLE_DEVICES=3 python metrics_custom.py -m output/$CONFIG -t block_all_test
+# rendering and evaluation
+TEST_PATH="data/mill19/rubble-pixsfm/val"
+CUDA_VISIBLE_DEVICES=$(get_available_gpu) python render_large.py --config config/$CONFIG.yaml --custom_test $TEST_PATH
+CUDA_VISIBLE_DEVICES=$(get_available_gpu) python metrics_custom.py -m output/$CONFIG -t val
