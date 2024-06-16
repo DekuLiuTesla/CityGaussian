1c488735d2e9083b3f9ef73e6c41018def13f231
DekuLiuTesla <1369939686@qq.com>
2024-06-16 13:11:03 +0800

    adds data partitioning

---
+++ internal/utils/blocking.py
@@ -0,0 +1,131 @@
+import torch
+
+def contract_to_unisphere(
+    x: torch.Tensor,
+    aabb: torch.Tensor,
+    ord: float = 2,
+    eps: float = 1e-6,
+    derivative: bool = False,
+):
+    aabb_min, aabb_max = torch.split(aabb, 3, dim=-1)
+    x = (x - aabb_min) / (aabb_max - aabb_min)
+    x = x * 2 - 1  # aabb is at [-1, 1]
+    mag = torch.linalg.norm(x, ord=ord, dim=-1, keepdim=True)
+    mask = mag.squeeze(-1) > 1
+
+    if derivative:
+        dev = (2 * mag - 1) / mag**2 + 2 * x**2 * (
+            1 / mag**3 - (2 * mag - 1) / mag**4
+        )
+        dev[~mask] = 1.0
+        dev = torch.clamp(dev, min=eps)
+        return dev
+    else:
+        x[mask] = (2 - 1 / mag[mask]) * (x[mask] / mag[mask])
+        x = x / 4 + 0.5  # [-inf, inf] is at [0, 1]
+        return x
+
+def block_filtering(block_id, xyz_org, aabb, block_dim, scale=1.0, mask_only=True):
+
+    if len(aabb) == 4:
+        aabb = [aabb[0], aabb[1], xyz_org[:, -1].min(), 
+                aabb[2], aabb[3], xyz_org[:, -1].max()]
+    elif len(aabb) == 6:
+        aabb = aabb
+    else:
+        assert False, "Unknown aabb format!"
+
+    xyz_tensor = torch.tensor(xyz_org)
+    aabb = torch.tensor(aabb, dtype=torch.float32, device=xyz_tensor.device)
+    block_id_z = block_id // (block_dim[0] * block_dim[1]);
+    block_id_y = (block_id % (block_dim[0] * block_dim[1])) // block_dim[0];
+    block_id_x = (block_id % (block_dim[0] * block_dim[1])) % block_dim[0];
+
+    xyz = contract_to_unisphere(xyz_tensor, aabb, ord=torch.inf)
+    min_x, max_x = float(block_id_x) / block_dim[0], float(block_id_x + 1) / block_dim[0]
+    min_y, max_y = float(block_id_y) / block_dim[1], float(block_id_y + 1) / block_dim[1]
+    min_z, max_z = float(block_id_z) / block_dim[2], float(block_id_z + 1) / block_dim[2]
+
+    delta_x = (max_x - min_x) * (scale - 1.0)
+    delta_y = (max_y - min_y) * (scale - 1.0)
+    delta_z = (max_z - min_z) * (scale - 1.0)
+
+    min_x -= delta_x / 2
+    max_x += delta_x / 2
+    min_y -= delta_y / 2
+    max_y += delta_y / 2
+    min_z -= delta_z / 2
+    
+    block_mask = (xyz[:, 0] >= min_x) & (xyz[:, 0] < max_x)  \
+                    & (xyz[:, 1] >= min_y) & (xyz[:, 1] < max_y) \
+                    & (xyz[:, 2] >= min_z) & (xyz[:, 2] < max_z)
+
+    if mask_only:
+        return block_mask
+    else:
+        return mask_only, xyz, [min_x, max_x, min_y, max_y, min_z, max_z]
+
+def which_block(xyz_org, aabb, block_dim):
+
+    if len(aabb) == 4:
+        aabb = [aabb[0], aabb[1], xyz_org[:, -1].min(), 
+                aabb[2], aabb[3], xyz_org[:, -1].max()]
+    elif len(aabb) == 6:
+        aabb = aabb
+    else:
+        assert False, "Unknown aabb format!"
+
+    xyz_tensor = torch.tensor(xyz_org)
+    aabb = torch.tensor(aabb, dtype=torch.float32, device=xyz_tensor.device)
+
+    xyz = contract_to_unisphere(xyz_tensor, aabb, ord=torch.inf)
+    block_id = torch.zeros(xyz.shape[0], dtype=torch.int32, device=xyz.device)
+    block_id_x = torch.floor(xyz[:, 0] * block_dim[0]).clamp(0, block_dim[0] - 1).int()
+    block_id_y = torch.floor(xyz[:, 1] * block_dim[1]).clamp(0, block_dim[1] - 1).int()
+    block_id_z = torch.floor(xyz[:, 2] * block_dim[2]).clamp(0, block_dim[2] - 1).int()
+
+    block_id = block_id_z * block_dim[0] * block_dim[1] + block_id_y * block_dim[0] + block_id_x
+
+    return block_id
+
+def in_frustum(viewpoint_cam, cell_corners, aabb, block_dim):
+    num_cell = cell_corners.shape[0]
+    device = cell_corners.device
+
+    cell_corners = torch.cat([cell_corners, torch.ones_like(cell_corners[..., [0]])], dim=-1)
+    cam_center = viewpoint_cam.camera_center
+    full_proj_transform = viewpoint_cam.full_proj_transform.repeat(num_cell, 1, 1)
+    viewmatrix = viewpoint_cam.world_view_transform.repeat(num_cell, 1, 1)
+    cell_corners_screen = cell_corners.bmm(full_proj_transform)
+    cell_corners_screen = cell_corners_screen / cell_corners_screen[..., [-1]]
+    cell_corners_screen = cell_corners_screen[..., :-1].reshape(-1, 3)
+
+    cell_corners_cam = cell_corners.bmm(viewmatrix)
+    mask = (cell_corners_cam[..., 2] > 0.2)
+
+    mask_ = mask.reshape(-1)
+    cell_corners_screen_ = cell_corners_screen.clone().reshape(-1, 3)
+    cell_corners_screen_[~mask_] = torch.inf
+    cell_corners_screen_min = cell_corners_screen_.reshape(num_cell, -1, 3).min(dim=1).values
+    cell_corners_screen_min[cell_corners_screen_min==torch.inf] = 0.0
+
+    cell_corners_screen_ = cell_corners_screen.clone().reshape(-1, 3)
+    cell_corners_screen_[~mask_] = -torch.inf
+    cell_corners_screen_max = cell_corners_screen_.reshape(num_cell, -1, 3).max(dim=1).values
+    cell_corners_screen_max[cell_corners_screen_max==-torch.inf] = 0.0
+
+    box_a = torch.cat([cell_corners_screen_min[:, :2], cell_corners_screen_max[:, :2]], dim=1)
+    box_b = torch.tensor([[-1, -1, 1, 1]], dtype=torch.float32, device=device)
+    A = box_a.size(0)
+    B = box_b.size(0)
+    max_xy = torch.min(box_a[:, 2:].unsqueeze(1).expand(A, B, 2),
+                    box_b[:, 2:].unsqueeze(0).expand(A, B, 2))
+    min_xy = torch.max(box_a[:, :2].unsqueeze(1).expand(A, B, 2),
+                    box_b[:, :2].unsqueeze(0).expand(A, B, 2))
+    inter = torch.clamp((max_xy - min_xy), min=0)
+    mask = (inter[:, 0, 0] * inter[:, 0, 1]) > 0
+
+    cam_center_id = which_block(cam_center[None, :], aabb, block_dim)[0]
+    mask[cam_center_id] = True
+
+    return mask
---
+++ tools/data_partition.py
@@ -0,0 +1,172 @@
+import os
+import sys
+import yaml
+import torch
+from tqdm import tqdm
+from argparse import ArgumentParser, Namespace
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from internal.utils.ssim import ssim
+from internal.utils.blocking import contract_to_unisphere
+from internal.utils.gaussian_model_loader import GaussianModelLoader
+from internal.dataparsers.colmap_dataparser import ColmapDataParser
+
+def parse(data):
+    data = Namespace(**data)
+    for arg in vars(data):
+        if isinstance(getattr(data, arg), dict):
+            setattr(data, arg, parse(getattr(data, arg)))
+    return data
+
+def block_filtering(gaussians, 
+                    renderer, 
+                    dataset, 
+                    save_dir, 
+                    block_dim, 
+                    aabb, 
+                    num_threshold, 
+                    content_threshold, 
+                    background_color, 
+                    quiet=False, 
+                    disable_inblock=False):
+
+        xyz_org = gaussians.get_xyz
+        block_num = block_dim[0] * block_dim[1] * block_dim[2]
+        bg_color=torch.tensor(background_color, dtype=torch.float, device="cuda")
+
+        if aabb is None:
+            with torch.no_grad():
+                sorted_x = torch.sort(xyz_org[::100, 0], descending=True)[0]
+                sorted_y = torch.sort(xyz_org[::100, 1], descending=True)[0]
+                sorted_z = torch.sort(xyz_org[::100, 2], descending=True)[0]
+
+                ratio = 0.999
+                x_max = torch.quantile(sorted_x, ratio)
+                x_min = torch.quantile(sorted_x, 1-ratio)
+                y_max = torch.quantile(sorted_y, ratio)
+                y_min = torch.quantile(sorted_y, 1-ratio)
+                z_max = torch.quantile(sorted_z, ratio)
+                z_min = torch.quantile(sorted_z, 1-ratio)
+
+                xyz_min = torch.stack([x_min, y_min, z_min])
+                xyz_max = torch.stack([x_max, y_max, z_max])
+                central_min = xyz_min + (xyz_max - xyz_min) / 3
+                central_max = xyz_max - (xyz_max - xyz_min) / 3
+
+                aabb = torch.cat([central_min, central_max], dim=-1)
+        else:
+            if len(aabb) == 4:
+                aabb = [aabb[0], aabb[1], xyz_org[:, -1].min(), 
+                        aabb[2], aabb[3], xyz_org[:, -1].max()]
+            elif len(aabb) == 6:
+                aabb = aabb
+            else:
+                assert False, "Unknown aabb format!"
+            aabb = torch.tensor(aabb, dtype=torch.float32, device=xyz_org.device)
+        
+        print(f"Block number: {block_num}, Gaussian number threshold: {num_threshold}")
+
+        block_image_list = {i: [] for i in range(block_num)}
+        
+        with torch.no_grad():
+
+            for block_id in range(block_num):
+                block_id_z = block_id // (block_dim[0] * block_dim[1])
+                block_id_y = (block_id % (block_dim[0] * block_dim[1])) // block_dim[0]
+                block_id_x = (block_id % (block_dim[0] * block_dim[1])) % block_dim[0]
+
+                xyz = contract_to_unisphere(xyz_org, aabb, ord=torch.inf)
+                min_x, max_x = float(block_id_x) / block_dim[0], float(block_id_x + 1) / block_dim[0]
+                min_y, max_y = float(block_id_y) / block_dim[1], float(block_id_y + 1) / block_dim[1]
+                min_z, max_z = float(block_id_z) / block_dim[2], float(block_id_z + 1) / block_dim[2]
+
+                num_gs, org_min_x, org_max_x, org_min_y, org_max_y, org_min_z, org_max_z = 0, min_x, max_x, min_y, max_y, min_z, max_z
+
+                while num_gs < num_threshold:
+                    # TODO: select better threshold
+                    block_mask = (xyz[:, 0] >= min_x) & (xyz[:, 0] < max_x)  \
+                                & (xyz[:, 1] >= min_y) & (xyz[:, 1] < max_y) \
+                                & (xyz[:, 2] >= min_z) & (xyz[:, 2] < max_z)
+                    num_gs = block_mask.sum()
+                    min_x -= 0.01
+                    max_x += 0.01
+                    min_y -= 0.01
+                    max_y += 0.01
+                    min_z -= 0.01
+                    max_z += 0.01
+                
+                print(f"\nStart filtering block {block_id} with {num_gs} gaussians.")
+                with open(os.path.join(save_dir, f"block_{block_id}.txt"), "w") as f:
+                    for idx in tqdm(range(len(dataset.cameras))):
+                        camera = dataset.cameras[idx].to_device("cuda")
+                        contract_cam_center = contract_to_unisphere(camera.camera_center, aabb, ord=torch.inf)
+
+                        if (not disable_inblock) and contract_cam_center[0] > org_min_x and contract_cam_center[0] < org_max_x \
+                            and contract_cam_center[1] > org_min_y and contract_cam_center[1] < org_max_y \
+                            and contract_cam_center[2] > org_min_z and contract_cam_center[2] < org_max_z :
+                            f.write(f"{dataset.image_names[idx]}\n")
+                            block_image_list[block_id].append(dataset.image_names[idx])
+                            continue
+
+                        img_org_gs = renderer(camera, gaussians, bg_color=bg_color)["render"]
+                        gaussians.select(block_mask)  # disable gaussians within block
+                        img_masked_gs = renderer(camera, gaussians, bg_color=bg_color)["render"]
+                        gaussians._opacity = gaussians._opacity_origin  # recover opacity
+
+                        loss = 1.0 - ssim(img_masked_gs, img_org_gs)
+                        if loss > content_threshold:
+                            f.write(f"{dataset.image_names[idx]}\n")
+                            block_image_list[block_id].append(dataset.image_names[idx])
+        
+        if not quiet:
+            for block_id in range(block_num):
+                print(f"Block {block_id} / {block_num} has {len(block_image_list[block_id])} cameras.")
+                    
+
+if __name__ == "__main__":
+    # Set up command line argument parser
+    parser = ArgumentParser(description="Training script parameters")
+    parser.add_argument('--model_path', type=str, help='path of coarse global model', required=True)
+    parser.add_argument("--block_dim", type=int, nargs="+", required=True)
+    parser.add_argument("--aabb", type=float, nargs="+", default=None)
+    parser.add_argument("--num_threshold", type=int, default=25000)
+    parser.add_argument("--content_threshold", type=float, default=0.08)
+    parser.add_argument("--save_dir", type=str, help="directory to save partition", default=None)
+    parser.add_argument("--quiet", action="store_true")
+    parser.add_argument("--disable_inblock", action="store_true")
+    args = parser.parse_args(sys.argv[1:])
+    
+    model, renderer = GaussianModelLoader.search_and_load(
+        args.model_path,
+        sh_degree=3,
+        device="cuda",
+    )
+    print("Gaussian count: {}".format(model.get_xyz.shape[0]))
+
+    config_path = os.path.join(args.model_path, "config.yaml")
+    with open(config_path, 'r') as f:
+        config = parse(yaml.load(f, Loader=yaml.FullLoader))
+    
+    if args.save_dir is None:
+        save_dir = os.path.join(config.data.path, "partition", f"{args.block_dim[0]}_{args.block_dim[1]}_{args.block_dim[2]}")
+    else:
+        save_dir = args.save_dir
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+            
+    dataparser_outputs = ColmapDataParser(
+        os.path.expanduser(config.data.path),
+        os.path.abspath(""),
+        global_rank=0,
+        params=config.data.params.colmap,
+    ).get_outputs()
+
+    block_filtering(model, renderer, 
+                    dataparser_outputs.train_set, 
+                    save_dir, args.block_dim, args.aabb, 
+                    args.num_threshold, args.content_threshold,
+                    config.model.background_color, 
+                    disable_inblock=False)
+
+    # All done
+    print("Partition complete.")
