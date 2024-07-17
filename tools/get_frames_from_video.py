87026e97c96747248f4d24b90e75f4c3aacec130
DekuLiuTesla <1369939686@qq.com>
2024-07-17 20:24:24 +0800

    adds resolution and delete start_idx for video2frame

--- tools/get_frames_from_video.py
+++ tools/get_frames_from_video.py
@@ -6,7 +6,7 @@ import sys
 from tqdm import tqdm
 from argparse import ArgumentParser
  
-def get_frame_from_video(video_paths, save_path, interval, start_idx):
+def get_frame_from_video(video_paths, save_path, interval, downsample_factor):
 
     if save_path is None:
         save_path = video_paths[0].split(".")[0] + "_frames/input/"
@@ -19,20 +19,20 @@ def get_frame_from_video(video_paths, save_path, interval, start_idx):
         os.makedirs(save_path)
         print('path of %s already exist and rebuild' % save_path)
     
-    start_idx = 0
     for video_path in video_paths:
         
         video_capture = cv2.VideoCapture(video_path)
+        video_name = video_path.split("/")[-1].split(".")[0]
         
         for i in tqdm(range(0, int(video_capture.get(7)), interval), desc=f"Extracting frames from {video_path}"):
             video_capture.set(cv2.CAP_PROP_POS_FRAMES, i)
             success, frame = video_capture.read()
+            if downsample_factor > 0:
+                frame = cv2.resize(frame, (frame.shape[1] // downsample_factor, frame.shape[0] // downsample_factor))
             if success:
-                cv2.imwrite(save_path + str(i+start_idx) + '.jpg', frame)
+                cv2.imwrite(save_path + f'{video_name}_{i}.jpg', frame)
             else:
                 break
-        
-        start_idx = i + start_idx + interval
  
  
 if __name__ == '__main__':
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     parser.add_argument("--video_paths", type=str, required=True, nargs="+")
     parser.add_argument("--save_path", type=str, default=None)
     parser.add_argument("--frame_interval", type=int, default=15)
-    parser.add_argument("--start_idx", type=int, default=0)
+    parser.add_argument("--downsample_factor", type=int, default=-1)
 
     args = parser.parse_args(sys.argv[1:])
-    get_frame_from_video(args.video_paths, args.save_path, args.frame_interval, args.start_idx)
+    get_frame_from_video(args.video_paths, args.save_path, args.frame_interval, args.downsample_factor)
