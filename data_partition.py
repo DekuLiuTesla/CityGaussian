284adb88c3de90c85bc304c2a54fbdefb27c1230
DekuLiuTesla <1369939686@qq.com>
2024-07-17 09:16:56 +0800

    adds data_partition with higher efficiency

--- data_partition.py
+++ data_partition.py
@@ -17,6 +17,14 @@ from utils.loss_utils import ssim
 from utils.camera_utils import loadCam
 from arguments import GroupParams
 
+def focus_point_fn(poses: np.ndarray) -> np.ndarray:
+    """Calculate nearest point to all focal axes in poses."""
+    directions, origins = poses[:, :3, 2:3], poses[:, :3, 3:4]
+    m = np.eye(3) - directions * np.transpose(directions, [0, 2, 1])
+    mt_m = np.transpose(m, [0, 2, 1]) @ m
+    focus_pt = np.linalg.inv(mt_m.mean(0)) @ (mt_m @ origins).mean(0)[:, 0]
+    return focus_pt
+
 def block_filtering(cameras, gaussians, args, pp, scale=1.0, quiet=False, disable_inblock=False, simple_selection=False):
         if not hasattr(args, 'filter_mode'):
             args.filter_mode = 'opacity'
@@ -24,70 +32,85 @@ def block_filtering(cameras, gaussians, args, pp, scale=1.0, quiet=False, disabl
             assert args.filter_mode in ['opacity', 'loss'], "Unknown filter mode!"
 
         xyz_org = gaussians.get_xyz
-        if not hasattr(args, 'aabb'):
-            with torch.no_grad():
-                sorted_x = torch.sort(xyz_org[::100, 0], descending=True)[0]
-                sorted_y = torch.sort(xyz_org[::100, 1], descending=True)[0]
-                sorted_z = torch.sort(xyz_org[::100, 2], descending=True)[0]
-
-                ratio = 0.999
-                x_max = torch.quantile(sorted_x, ratio)
-                x_min = torch.quantile(sorted_x, 1-ratio)
-                y_max = torch.quantile(sorted_y, ratio)
-                y_min = torch.quantile(sorted_y, 1-ratio)
-                z_max = torch.quantile(sorted_z, ratio)
-                z_min = torch.quantile(sorted_z, 1-ratio)
-
-                xyz_min = torch.stack([x_min, y_min, z_min])
-                xyz_max = torch.stack([x_max, y_max, z_max])
-                central_min = xyz_min + (xyz_max - xyz_min) / 3
-                central_max = xyz_max - (xyz_max - xyz_min) / 3
-
-                aabb = torch.cat([central_min, central_max], dim=-1)
-        else:
-            if len(args.aabb) == 4:
-                aabb = [args.aabb[0], args.aabb[1], xyz_org[:, -1].min(), 
-                        args.aabb[2], args.aabb[3], xyz_org[:, -1].max()]
-            elif len(args.aabb) == 6:
-                aabb = args.aabb
-            else:
-                assert False, "Unknown aabb format!"
-            aabb = torch.tensor(aabb, dtype=torch.float32, device=xyz_org.device)
         block_num = args.block_dim[0] * args.block_dim[1] * args.block_dim[2]
         num_threshold = args.num_threshold if hasattr(args, 'num_threshold') else 25000
+
+        if args.aabb is None:
+            torch.cuda.empty_cache()
+            c2ws = np.array([np.linalg.inv(np.asarray((cam.world_to_camera.T).cpu().numpy())) for cam in cameras])
+            poses = c2ws[:,:3,:] @ np.diag([1, -1, -1, 1])
+            center = (focus_point_fn(poses))
+            radius = torch.tensor(np.median(np.abs(c2ws[:,:3,3] - center), axis=0), device=xyz_org.device)
+            center = torch.from_numpy(center).float().to(xyz_org.device)
+            if radius.min() / radius.max() < 0.02:
+                # If the radius is too small, we don't contract in this dimension
+                radius[torch.argmin(radius)] = 0.5 * (xyz_org[:, torch.argmin(radius)].max() - xyz_org[:, torch.argmin(radius)].min())
+            args.aabb = torch.zeros(6, device=xyz_org.device)
+            args.aabb[:3] = center - radius
+            args.aabb[3:] = center + radius
+        else:
+            assert len(args.aabb) == 6, "Unknown args.aabb format!"
+            args.aabb = torch.tensor(args.aabb, dtype=torch.float32, device=xyz_org.device)
+        
         print(f"Block number: {block_num}, Gaussian number threshold: {num_threshold}")
 
         camera_mask = torch.zeros((len(cameras), block_num), dtype=torch.bool, device=xyz_org.device)
         
         with torch.no_grad():
-            for idx in tqdm(range(len(cameras))):
-                bg_color = [1,1,1] if args.white_background else [0, 0, 0]
-                background = torch.tensor(bg_color, dtype=torch.float32, device=xyz_org.device)
-                c = cameras[idx]
-                viewpoint_cam = loadCam(args, idx, c, scale)
-                contract_cam_center = contract_to_unisphere(viewpoint_cam.camera_center, aabb, ord=torch.inf)
 
-                for block_id in range(block_num):
-                    block_id_z = block_id // (args.block_dim[0] * args.block_dim[1])
-                    block_id_y = (block_id % (args.block_dim[0] * args.block_dim[1])) // args.block_dim[0]
-                    block_id_x = (block_id % (args.block_dim[0] * args.block_dim[1])) % args.block_dim[0]
+            for block_id in range(block_num):
+                block_id_z = block_id // (args.block_dim[0] * args.block_dim[1])
+                block_id_y = (block_id % (args.block_dim[0] * args.block_dim[1])) // args.block_dim[0]
+                block_id_x = (block_id % (args.block_dim[0] * args.block_dim[1])) % args.block_dim[0]
 
-                    if hasattr(args, 'xyz_limited') and args.xyz_limited:
-                        xyz = xyz_org
-                        min_x = aabb[0] + (aabb[3] - aabb[0]) * float(block_id_x) / args.block_dim[0]
-                        max_x = aabb[0] + (aabb[3] - aabb[0]) * float(block_id_x + 1) / args.block_dim[0]
-                        min_y = aabb[1] + (aabb[4] - aabb[1]) * float(block_id_y) / args.block_dim[1]
-                        max_y = aabb[1] + (aabb[4] - aabb[1]) * float(block_id_y + 1) / args.block_dim[1]
-                        min_z = aabb[2] + (aabb[5] - aabb[2]) * float(block_id_z) / args.block_dim[2]
-                        max_z = aabb[2] + (aabb[5] - aabb[2]) * float(block_id_z + 1) / args.block_dim[2]
-                    else:
-                        xyz = contract_to_unisphere(xyz_org, aabb, ord=torch.inf)
-                        min_x, max_x = float(block_id_x) / args.block_dim[0], float(block_id_x + 1) / args.block_dim[0]
-                        min_y, max_y = float(block_id_y) / args.block_dim[1], float(block_id_y + 1) / args.block_dim[1]
-                        min_z, max_z = float(block_id_z) / args.block_dim[2], float(block_id_z + 1) / args.block_dim[2]
+                if hasattr(args, 'xyz_limited') and args.xyz_limited:
+                    xyz = xyz_org
+                    min_x = args.aabb[0] + (args.aabb[3] - args.aabb[0]) * float(block_id_x) / args.block_dim[0]
+                    max_x = args.aabb[0] + (args.aabb[3] - args.aabb[0]) * float(block_id_x + 1) / args.block_dim[0]
+                    min_y = args.aabb[1] + (args.aabb[4] - args.aabb[1]) * float(block_id_y) / args.block_dim[1]
+                    max_y = args.aabb[1] + (args.aabb[4] - args.aabb[1]) * float(block_id_y + 1) / args.block_dim[1]
+                    min_z = args.aabb[2] + (args.aabb[5] - args.aabb[2]) * float(block_id_z) / args.block_dim[2]
+                    max_z = args.aabb[2] + (args.aabb[5] - args.aabb[2]) * float(block_id_z + 1) / args.block_dim[2]
+                else:
+                    xyz = contract_to_unisphere(xyz_org, args.aabb, ord=torch.inf)
+                    min_x, max_x = float(block_id_x) / args.block_dim[0], float(block_id_x + 1) / args.block_dim[0]
+                    min_y, max_y = float(block_id_y) / args.block_dim[1], float(block_id_y + 1) / args.block_dim[1]
+                    min_z, max_z = float(block_id_z) / args.block_dim[2], float(block_id_z + 1) / args.block_dim[2]
 
-                    
-                    num_gs, org_min_x, org_max_x, org_min_y, org_max_y, org_min_z, org_max_z = 0, min_x, max_x, min_y, max_y, min_z, max_z
+                
+                num_gs, org_min_x, org_max_x, org_min_y, org_max_y, org_min_z, org_max_z = 0, min_x, max_x, min_y, max_y, min_z, max_z
+
+                while num_gs < num_threshold:
+                    # TODO: select better threshold
+                    block_mask = (xyz[:, 0] >= min_x) & (xyz[:, 0] < max_x)  \
+                                & (xyz[:, 1] >= min_y) & (xyz[:, 1] < max_y) \
+                                & (xyz[:, 2] >= min_z) & (xyz[:, 2] < max_z)
+                    num_gs = block_mask.sum()
+                    min_x -= 0.01
+                    max_x += 0.01
+                    min_y -= 0.01
+                    max_y += 0.01
+                    min_z -= 0.01
+                    max_z += 0.01
+                
+                block_mask = ~block_mask
+                sh_degree = gaussians.max_sh_degree
+                masked_gaussians = GaussianModel(sh_degree)
+                masked_gaussians._xyz = xyz_org[block_mask]
+                masked_gaussians._scaling = gaussians._scaling[block_mask]
+                masked_gaussians._rotation = gaussians._rotation[block_mask]
+                masked_gaussians._features_dc = gaussians._features_dc[block_mask]
+                masked_gaussians._features_rest = gaussians._features_rest[block_mask]
+                masked_gaussians._opacity = gaussians._opacity[block_mask]
+                masked_gaussians.max_radii2D = gaussians.max_radii2D[block_mask]
+
+                for idx in tqdm(range(len(cameras))):
+                    bg_color = [1,1,1] if args.white_background else [0, 0, 0]
+                    background = torch.tensor(bg_color, dtype=torch.float32, device=xyz_org.device)
+                    c = cameras[idx]
+                    viewpoint_cam = loadCam(args, idx, c, scale)
+                    contract_cam_center = contract_to_unisphere(viewpoint_cam.camera_center, args.aabb, ord=torch.inf)
+
                     if simple_selection > 1.0:
                         # enlarge the box to 1.5x
                         rate = (simple_selection - 1.0) / 2
@@ -102,30 +125,6 @@ def block_filtering(cameras, gaussians, args, pp, scale=1.0, quiet=False, disabl
                             and contract_cam_center[2] > min_z and contract_cam_center[2] < max_z :
                             camera_mask[idx, block_id] = True
                         continue
-
-                    while num_gs < num_threshold:
-                        # TODO: select better threshold
-                        block_mask = (xyz[:, 0] >= min_x) & (xyz[:, 0] < max_x)  \
-                                    & (xyz[:, 1] >= min_y) & (xyz[:, 1] < max_y) \
-                                    & (xyz[:, 2] >= min_z) & (xyz[:, 2] < max_z)
-                        num_gs = block_mask.sum()
-                        min_x -= 0.01
-                        max_x += 0.01
-                        min_y -= 0.01
-                        max_y += 0.01
-                        min_z -= 0.01
-                        max_z += 0.01
-                    
-                    block_mask = ~block_mask
-                    sh_degree = gaussians.max_sh_degree
-                    masked_gaussians = GaussianModel(sh_degree)
-                    masked_gaussians._xyz = xyz_org[block_mask]
-                    masked_gaussians._scaling = gaussians._scaling[block_mask]
-                    masked_gaussians._rotation = gaussians._rotation[block_mask]
-                    masked_gaussians._features_dc = gaussians._features_dc[block_mask]
-                    masked_gaussians._features_rest = gaussians._features_rest[block_mask]
-                    masked_gaussians._opacity = gaussians._opacity[block_mask]
-                    masked_gaussians.max_radii2D = gaussians.max_radii2D[block_mask]
 
                     if (not disable_inblock) and contract_cam_center[0] > org_min_x and contract_cam_center[0] < org_max_x \
                         and contract_cam_center[1] > org_min_y and contract_cam_center[1] < org_max_y \
