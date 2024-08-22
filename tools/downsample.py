e6f2115c827b0cf4cb28db3003cec7044bf818a0
DekuLiuTesla <1369939686@qq.com>
2024-08-22 13:18:09 +0800

    adds point cloud downsampling tool

---
+++ tools/downsample.py
@@ -0,0 +1,25 @@
+import os
+import sys
+sys.path.append('/home/yang_liu/python_workspace/gaussian-splatting-lightning')
+
+import numpy as np
+import open3d as o3d
+from argparse import ArgumentParser
+
+if __name__ == "__main__":
+    # Set up command line argument parser
+    parser = ArgumentParser(description="Training script parameters")
+    parser.add_argument('--file_dir', '-f', type=str, help='path to target point cloud', required=True)
+    parser.add_argument('--vox_size', '-v', type=float, help='downsampling voxel size', default=0.008)
+    args = parser.parse_args(sys.argv[1:])
+    
+    pcd = o3d.io.read_point_cloud(args.file_dir)
+    print(f"{args.file_dir} has {len(pcd.points)} points")
+
+    ds_pcd = pcd.voxel_down_sample(voxel_size=args.vox_size)
+    print("Downsampled has", len(ds_pcd.points), "points")
+    print(np.mean(ds_pcd.compute_nearest_neighbor_distance()))
+
+    save_dir = args.file_dir.replace(".ply", "_ds.ply")
+    print(save_dir)
+    o3d.io.write_point_cloud(save_dir, ds_pcd)
