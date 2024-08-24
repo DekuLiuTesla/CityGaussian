d50f8af14d4ef3b8c4f11f2f91ee006f9e9ce024
DekuLiuTesla <1369939686@qq.com>
2024-08-24 15:24:07 +0800

    updates blender script and geometric eval for mc

--- blender/scripts/render_mesh_mc_aerial.sh
+++ blender/scripts/render_mesh_mc_aerial.sh
@@ -1,17 +1,32 @@
 # Function to get the id of an available GPU
 get_available_gpu() {
-  local mem_threshold=5000
+  local mem_threshold=500
   nvidia-smi --query-gpu=index,memory.used --format=csv,noheader,nounits | awk -v threshold="$mem_threshold" -F', ' '
   $2 < threshold { print $1; exit }
   '
 }
 
-COARSE_NAME=citygs2d_mc_aerial_coarse
-NAME=citygs2d_mc_aerial_5x5
+COARSE_NAME=citygs_flatten_mc_aerial_coarse
+NAME=citygs_flatten_mc_aerial
 # gpu_id=$(get_available_gpu)
 # echo "GPU $gpu_id is available."
-# CUDA_VISIBLE_DEVICES=$gpu_id python mesh.py --model_path outputs/$NAME --config_path outputs/$COARSE_NAME/config.yaml --voxel_size 0.015 --sdf_trunc 0.1 --depth_trunc 10
+# CUDA_VISIBLE_DEVICES=$gpu_id python mesh.py --model_path outputs/$COARSE_NAME --config_path outputs/$COARSE_NAME/config.yaml --voxel_size 0.01 --sdf_trunc 0.04 --depth_trunc 10
 
+gpu_id=$(get_available_gpu)
+echo "GPU $gpu_id is available."
+CUDA_VISIBLE_DEVICES=$gpu_id python mesh.py --model_path outputs/$NAME --config_path outputs/$COARSE_NAME/config.yaml --voxel_size 0.01 --sdf_trunc 0.04 --depth_trunc 10
+
+# COARSE_NAME=citygs2d_mc_aerial_coarse
+# NAME=citygs2d_mc_aerial_5x5
+
+# gpu_id=$(get_available_gpu)
+# echo "GPU $gpu_id is available."
+# CUDA_VISIBLE_DEVICES=$gpu_id python mesh.py --model_path outputs/$COARSE_NAME --config_path outputs/$COARSE_NAME/config.yaml --voxel_size 0.01 --sdf_trunc 0.04 --depth_trunc 10
+
+# gpu_id=$(get_available_gpu)
+# echo "GPU $gpu_id is available."
+# CUDA_VISIBLE_DEVICES=$gpu_id python mesh.py --model_path outputs/$NAME --config_path outputs/$COARSE_NAME/config.yaml --voxel_size 0.01 --sdf_trunc 0.04 --depth_trunc 10 &
+# sleep 120
 
 # python tools/generate_traj.py --config outputs/$COARSE_NAME/config.yaml \
 #                               --mesh_path "outputs/$NAME/mesh/merged_epoch=6-step=30000" \
@@ -21,11 +36,11 @@ NAME=citygs2d_mc_aerial_5x5
 # cd blender
 # conda activate blender34
 
-gpu_id=$(get_available_gpu)
-echo "GPU $gpu_id is available."
-CUDA_VISIBLE_DEVICES=3 python render_sun.py \
-                     --load_dir "../outputs/$NAME/mesh/merged_epoch=6-step=30000" \
-                     --mesh_file fuse_sdf_0.1_vox_15_filtered.ply \
-                     --config_dir render_cfgs/mc \
+# gpu_id=$(get_available_gpu)
+# echo "GPU $gpu_id is available."
+# CUDA_VISIBLE_DEVICES=3 python render_sun.py \
+#                      --load_dir "../outputs/$NAME/mesh/merged_epoch=6-step=30000" \
+#                      --mesh_file fuse_sdf_0.1_vox_15_filtered.ply \
+#                      --config_dir render_cfgs/mc \
 
 # cd ..
--- tools/eval_tnt/run_mc.py
+++ tools/eval_tnt/run_mc.py
@@ -99,7 +99,7 @@ def run_evaluation(scene, dataset_dir, ply_path, out_dir, view_crop):
     
     print(gt_filen)
     gt_pcd = o3d.io.read_point_cloud(gt_filen)
-    dTau = np.mean(gt_pcd.compute_nearest_neighbor_distance()) * 2.5  # around 5e-4 of scene size
+    dTau = np.mean(gt_pcd.compute_nearest_neighbor_distance()) * 1.5  # around 5e-4 of scene size
     
     # big pointclouds will be downlsampled to this number to speed up alignment
     dist_threshold = dTau
