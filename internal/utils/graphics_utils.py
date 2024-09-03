ce7306c884aed384f16d58d0021f21c868a0e589
DekuLiuTesla <1369939686@qq.com>
2024-09-03 10:04:33 +0800

    adds support for pcd with no normal in fetch_ply

--- internal/utils/graphics_utils.py
+++ internal/utils/graphics_utils.py
@@ -27,7 +27,10 @@ def fetch_ply(path):
     vertices = plydata['vertex']
     positions = np.vstack([vertices['x'], vertices['y'], vertices['z']]).T
     colors = np.vstack([vertices['red'], vertices['green'], vertices['blue']]).T / 255.0
-    normals = np.vstack([vertices['nx'], vertices['ny'], vertices['nz']]).T
+    if 'nx' not in vertices:
+        normals = np.zeros_like(positions)
+    else:
+        normals = np.vstack([vertices['nx'], vertices['ny'], vertices['nz']]).T
     return BasicPointCloud(points=positions, colors=colors, normals=normals)
 
 
