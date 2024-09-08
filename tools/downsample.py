8ceedd0c88c0e19c39dc16fbe392257777bb1b24
DekuLiuTesla <1369939686@qq.com>
2024-09-08 11:05:28 +0800

    adds scaling factor to downsample tool

--- tools/downsample.py
+++ tools/downsample.py
@@ -11,12 +11,13 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="Training script parameters")
     parser.add_argument('--file_dir', '-f', type=str, help='path to target point cloud', required=True)
     parser.add_argument('--vox_size', '-v', type=float, help='downsampling voxel size', default=0.008)
+    parser.add_argument('--scaling_factor', '-s', type=float, help='position scaling factor', default=1.0)
     args = parser.parse_args(sys.argv[1:])
     
     pcd = o3d.io.read_point_cloud(args.file_dir)
     print(f"{args.file_dir} has {len(pcd.points)} points")
 
-    ds_pcd = pcd.voxel_down_sample(voxel_size=args.vox_size)
+    ds_pcd = pcd.voxel_down_sample(voxel_size=args.vox_size).scale(args.scaling_factor, center=(0, 0, 0))
     print("Downsampled has", len(ds_pcd.points), "points")
     print(np.mean(ds_pcd.compute_nearest_neighbor_distance()))
 
