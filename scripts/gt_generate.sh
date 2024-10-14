c733f5cdd6cead61bcc41003f8cd41f73d31d739
DekuLiuTesla <1369939686@qq.com>
2024-10-14 22:03:28 +0800

    updates ellipse trajectory generation

--- scripts/gt_generate.sh
+++ scripts/gt_generate.sh
@@ -9,3 +9,9 @@ python tools/generate_traj.py --config outputs/citygs2d_smbu_coarse_lnorm4_wo_va
 python tools/generate_traj.py --config outputs/citygs2d_upper_coarse_lnorm4_wo_vast_sep_depth_init_5/config.yaml
 python tools/generate_traj.py --config outputs/citygs2d_mc_aerial_coarse_lnorm4_wo_vast_sep_depth_init_5/config.yaml --data_path data/matrix_city/aerial/test/block_all_test --train
 python tools/generate_traj.py --config outputs/citygs2d_mc_street_coarse_lnorm4_wo_vast_sep_depth_init_5_ubd1e1/config.yaml --data_path data/matrix_city/street/test/block_A_test --train
+
+python tools/generate_traj.py --config outputs/citygs2d_lfls_coarse_lnorm4_wo_vast_sep_depth_init_5/config.yaml --train --ellipse --scale_percentile 75 --n_fames 480
+python tools/generate_traj.py --config outputs/citygs2d_smbu_coarse_lnorm4_wo_vast_sep_depth_init_5/config.yaml --train --ellipse --scale_percentile 75 --n_fames 480
+python tools/generate_traj.py --config outputs/citygs2d_upper_coarse_lnorm4_wo_vast_sep_depth_init_5/config.yaml --train --ellipse --scale_percentile 75 --n_fames 480
+python tools/generate_traj.py --config outputs/citygs2d_mc_aerial_coarse_lnorm4_wo_vast_sep_depth_init_5/config.yaml --data_path data/matrix_city/aerial/train/block_all --scale_percentile 75 --ellipse
+# python tools/generate_traj.py --config outputs/citygs2d_mc_street_coarse_lnorm4_wo_vast_sep_depth_init_5_ubd1e1/config.yaml --data_path data/matrix_city/street/train/block_A --ellipse
